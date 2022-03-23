83dc589fc26290ffd3639597454ac5eaf7990bab
BenSeymourODB <ben.seymour@odb.org>
2022-03-23 10:43:28 -0400

    Tutorial code + .NET 6 Program + SqlServer

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+//var builder = WebApplication.CreateBuilder(args);
+var builder = CreateHostBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<ElsaQuickstarts.Server.ApiEndpoints.Startup>();
+                });
